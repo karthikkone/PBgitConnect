9260ed4a2406636f3a67a91dda422132794791a6
karthikkone <mohit@Mohits-MacBook-Air.local>
2018-10-25 02:02:37 +0530

    add POST /{owner}/repos/{repository}/refs/heads to create new branch

--- src/main/java/com/agp/pbgit/service/GitAPIserviceImpl.java
+++ src/main/java/com/agp/pbgit/service/GitAPIserviceImpl.java
@@ -11,10 +11,7 @@ import com.agp.pbgit.model.RevisionModel;
 import com.agp.pbgit.model.db.AuthData;
 import com.agp.pbgit.service.db.AuthDataRepository;
 
-import org.kohsuke.github.GHCreateRepositoryBuilder;
-import org.kohsuke.github.GHRef;
-import org.kohsuke.github.GHRepository;
-import org.kohsuke.github.GitHub;
+import org.kohsuke.github.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,7 +24,7 @@ import org.springframework.web.bind.annotation.*;
  * GitAPIserviceImpl a REST API wrapper around GitHub API
  */
 @RestController
-public class GitAPIserviceImpl implements GitAPIservice{
+public class GitAPIserviceImpl implements GitAPIservice {
     private String ghAuthToken;
     private AuthDataRepository authDataRepository;
     private final Logger logger = LoggerFactory.getLogger(GitAPIserviceImpl.class);
@@ -36,41 +33,41 @@ public class GitAPIserviceImpl implements GitAPIservice{
     public GitAPIserviceImpl(AuthDataRepository authDataRepository) {
         this.authDataRepository = authDataRepository;
     }
-    
-    
-    @RequestMapping(value="/{user}/repos", method=RequestMethod.GET)
+
+
+    @RequestMapping(value = "/{user}/repos", method = RequestMethod.GET)
     public List<RepoModel> listRepository(@PathVariable("user") String ghUser) throws IOException {
-    	
-    	//get stored Auth token
-    	AuthData ghAuthToken = authDataRepository.findAll().get(0);
 
-    	logger.info("using token with GitHub" + ghAuthToken);
+        //get stored Auth token
+        AuthData ghAuthToken = authDataRepository.findAll().get(0);
 
-    	//access github api with the token
+        logger.info("using token with GitHub" + ghAuthToken);
+
+        //access github api with the token
         GitHub github = GitHub.connectUsingOAuth(ghAuthToken.getAuthToken());
         Map<String, GHRepository> reps = github.getUser(ghUser).getRepositories();
         List<RepoModel> repoData = new LinkedList<RepoModel>();
-        
+
         for (GHRepository repo : reps.values()) {
-        	RepoModel model = new RepoModel();
-        	model.setRepoName(repo.getFullName());
-        	model.setOwnerName(repo.getOwnerName());
-        	model.setCreatedAt(repo.getCreatedAt());
-        	model.setMasterBranch(repo.getMasterBranch());
-        	model.setUpdatedAt(repo.getUpdatedAt());
-        	model.setUrl(repo.getHttpTransportUrl());
-        	
-        	repoData.add(model);
+            RepoModel model = new RepoModel();
+            model.setRepoName(repo.getFullName());
+            model.setOwnerName(repo.getOwnerName());
+            model.setCreatedAt(repo.getCreatedAt());
+            model.setMasterBranch(repo.getMasterBranch());
+            model.setUpdatedAt(repo.getUpdatedAt());
+            model.setUrl(repo.getHttpTransportUrl());
+
+            repoData.add(model);
         }
         return repoData;
     }
 
 
-    @RequestMapping(value="/{owner}/repos/{repository}/refs/heads", method = RequestMethod.GET)
-    public ResponseEntity<List<RevisionModel>> getRevisions(@PathVariable("owner") String owner, @PathVariable("repository") String repository) throws IOException{
+    @RequestMapping(value = "/{owner}/repos/{repository}/refs/heads", method = RequestMethod.GET)
+    public ResponseEntity<List<RevisionModel>> getRevisions(@PathVariable("owner") String owner, @PathVariable("repository") String repository) throws IOException {
         AuthData authData = authDataRepository.findAll().get(0);
 
-        logger.info("fetching revisions for "+owner+"/"+repository);
+        logger.info("fetching revisions for " + owner + "/" + repository);
 
         GitHub gitHub = GitHub.connectUsingOAuth(authData.getAuthToken());
         GHRef[] refs = gitHub.getUser(owner).getRepository(repository).getRefs();
@@ -80,11 +77,37 @@ public class GitAPIserviceImpl implements GitAPIservice{
         for (GHRef ref : refs) {
             if (ref != null && ref.getObject() != null) {
                 GHRef.GHObject refObj = ref.getObject();
-                revisions.add(new RevisionModel(refObj.getType(),refObj.getUrl(), refObj.getSha()));
+                revisions.add(new RevisionModel(refObj.getType(), refObj.getUrl(), refObj.getSha()));
             }
         }
 
         return new ResponseEntity<List<RevisionModel>>(revisions, HttpStatus.OK);
+    }
+
+
+    @RequestMapping(value = "/{owner}/repos/{repository}/refs/heads", method = RequestMethod.POST)
+    public ResponseEntity<String> createBranch(@PathVariable("owner") String owner, @PathVariable("repository") String repository, @RequestBody Map<String, Object> payload) throws  IOException {
+        AuthData authData = authDataRepository.findAll().get(0);
+        String sha = (String) payload.get("sha1");
+        String branch = (String) payload.get("branch");
+
+        logger.info("creating a branch @SHA-1 : " + sha + "/" + branch + " on repository " + owner + "/" + repository);
+        GitHub gitHub = GitHub.connectUsingOAuth(authData.getAuthToken());
+        GHRef[] refs = gitHub.getUser(owner).getRepository(repository).getRefs();
+
+        boolean ok = false;
+
+        for (GHRef ghRef : refs) {
+            if (ghRef != null && ghRef.getObject() != null && ghRef.getObject().getSha().equals(sha)) {
+                ok = true; //found the ref from where to spin off a branch
+            }
+        }
+
+        if (!ok) {
+            return new ResponseEntity<String>("message: no ref with sha found :" + sha, HttpStatus.NOT_FOUND);
+        }
+
+        return new ResponseEntity<String>("branch: " + branch, HttpStatus.OK);
     }
 
 }
