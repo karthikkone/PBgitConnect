8359827ca22c8b5a54ed22497134be5ea58bd9ed
Mohit Sharma <mohit.sharma41@ad.infosys.com>
2018-10-25 18:17:40 +0530

    added @EnableJpaRepositories

--- src/main/java/com/agp/pbgit/PbgitApplication.java
+++ src/main/java/com/agp/pbgit/PbgitApplication.java
@@ -2,8 +2,10 @@ package com.agp.pbgit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories("com.agp.pbgit.service.db")
 public class PbgitApplication {
 
 	public static void main(String[] args) {
