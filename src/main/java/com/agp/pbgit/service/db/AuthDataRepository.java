58ef035fcdad0cabb65f115b5fd54fd8b75c0161
Mohit Sharma <mohit.sharma41@ad.infosys.com>
2018-10-25 17:53:34 +0530

    extend AuthDataRepository by CrudRepository

--- src/main/java/com/agp/pbgit/service/db/AuthDataRepository.java
+++ src/main/java/com/agp/pbgit/service/db/AuthDataRepository.java
@@ -2,10 +2,12 @@ package com.agp.pbgit.service.db;
 
 
 import com.agp.pbgit.model.db.AuthData;
-import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AuthDataRepository extends JpaRepository<AuthData, Long>{
-
+public interface AuthDataRepository extends CrudRepository<AuthData, Long>{
+	List<AuthData> findAll();
 }
